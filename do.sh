4fdae381591a17a5d3e5b283b4446ef870037f67
imagesid <78056933+imagesid@users.noreply.github.com>
2022-05-17 17:20:27 +0900

    Add files via upload

---
+++ do.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+
+filename="leveldb/table/block.h"
+filename2="leveldb/table/block_builder.h"
+if [ "true" == "$2" ]; then
+	echo "==> SLR True ";
+	search="SLR = false";
+	replace="SLR = true";
+	sed -i "s/$search/$replace/" $filename
+	sed -i "s/$search/$replace/" $filename2
+	
+else
+	echo "==> SLR False ";
+	search="SLR = true";
+	replace="SLR = false";
+	sed -i "s/$search/$replace/" $filename
+	sed -i "s/$search/$replace/" $filename2
+fi 
+
+cd /home/leveldb/leveldb/build
+cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build .
+
+rm -rf /home/leveldb/dbonly/test5   
+
+cd /home/leveldb
+
+data=""
+if [ "osm" == "$1" ]; then
+	echo "==> Running OSM ";
+	g++ run.cc -Ileveldb/include -Lleveldb/build -lleveldb -lpthread -o run -g
+	./run 
+else
+	echo "==> Running Twitter User Data ";
+	g++ runtwitter.cc -Ileveldb/include -Lleveldb/build -lleveldb -lpthread -o runtwitter -g
+	./runtwitter 
+	data="twitter"
+fi
+
+cd /home/leveldb/leveldb/build
+
+./db_bench --benchmarks="readseqslr${data},readseqslr${data},readseqslr${data},readseqslr${data},readseqslr${data}" --histogram=1  --use_existing_db=1 --db=/home/leveldb/dbonly/test5
+
+
+
